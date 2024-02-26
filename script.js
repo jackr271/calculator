6a1793a0004c355517da077be980fbe88ee46379
Jack Roche <52017135+jackr271@users.noreply.github.com>
2024-02-26 16:24:07 -0500

    feat: build calc in html, add css style

--- script.js
+++ script.js
@@ -39,3 +39,15 @@ function multiply (num1, num2) {
 function divide (num1, num2) {
     return num1 / num2;
 }
+
+function operate (num1, num2, operand) {
+    switch (operand) {
+        case '+': return add(num1, num2);
+
+        case '-': return subtract(num1, num2);
+
+        case '*': return multiply(num1, num2);
+
+        case '/': return divide(num1, num2);
+    }
+}
