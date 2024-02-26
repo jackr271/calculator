a18efeddc9709dcc3e1f36607c7c6298c07f5b2b
Jack Roche <52017135+jackr271@users.noreply.github.com>
2024-02-26 15:27:00 -0500

    feat: build and link files, build math functions

---
+++ script.js
@@ -0,0 +1,41 @@
+/* 
+PSUEDOCODE
+
+FUNCTION add
+    INPUT number1, number2
+    RETURN number1 + number2
+
+FUNCTION subtract
+    INPUT number1, number2
+    RETURN number1 - number2
+
+FUNCTION multiply
+    INPUT number1, number2
+    RETURN number1 * number2
+
+FUNCTION divide
+    INPUT number1, number2
+    RETURN number1 / number2
+
+FUNCTION operate
+    INPUT number1, operand, number2
+        INIT solution
+
+        IF operand == * 
+*/
+
+function add (num1, num2) {
+    return num1 + num2;
+}
+
+function subtract (num1, num2) {
+    return num1 - num2;
+}
+
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+
+function divide (num1, num2) {
+    return num1 / num2;
+}
